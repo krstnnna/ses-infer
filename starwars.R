5fdd62f8fe0d47539fe754e5a015b6b1f09ed951
SooYoun Zong <sooyoun@SooYouns-Air.lan>
2024-01-31 22:33:57 -0600

    commit for hw 11 @nrdowling @ysh627

--- starwars.R
+++ starwars.R
@@ -18,3 +18,38 @@ sw.wrangled
 
 # Am I using the right built-in starwars dataset?
 # How do I import the the goal df to my data repo for comparison?
+
+
+# HW 11
+
+library(ggplot2)
+library(dplyr)
+
+starwars_data <- read.csv('sw-wrangled.csv')
+
+# Histogram of the height_cm column
+ggplot(starwars_data, aes(x = height_cm)) +
+  geom_histogram(binwidth = 10, fill = "grey", color = "black") +
+  labs(x = "height_cm", y = "count")
+
+
+# Histogram of hair colors sorted
+library(ggplot2)
+library(dplyr)
+
+# Histogram of hair color sorted
+starwars_data <- starwars_data %>% 
+  filter(!is.na(hair)) 
+starwars_data %>%
+  count(hair) %>% 
+  mutate(sorted_hair = reorder(hair, n)) %>%
+  ggplot(aes(x = sorted_hair, y = n)) +
+  geom_bar(stat = "identity", fill = "grey", color = "black") +
+  labs(x = "Hair Color", y = "Count")
+
+# Scatter plot of mass vs. height_in
+ggplot(starwars_data, aes(x = height_in, y = mass)) +
+  geom_point(shape = 17) +  # shape 17 is a filled triangle
+  labs(x = "height_in", y = "mass", ylim = c(0,50))
+
+# The outlier (1358) ? How should I manage it?
